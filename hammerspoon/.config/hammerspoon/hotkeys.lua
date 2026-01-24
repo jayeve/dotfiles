d3b5aad7935a76a224942e0583f7f5aa492f2d64
J Evans <j@cloudflare.com>
2026-01-24 04:31:38 -0800

    hammerflow

--- hammerspoon/.config/hammerspoon/hotkeys.lua
+++ hammerspoon/.config/hammerspoon/hotkeys.lua
@@ -5,70 +5,22 @@ local profiles = require("profiles")
 -- configured at https://github.com/jayeve/dotfiles/blob/2b1de320aeb019aad64f98fbb3f3863361efb9b3/.config/karabiner/karabiner.json#L9
 local hyper = { "ctrl", "alt", "shift", "cmd" }
 
-hs.hotkey.bind(hyper, "j", function()
-	hs.application.launchOrFocus(Apps.anki)
-end)
-
-hs.hotkey.bind(hyper, "p", function()
-	hs.application.launchOrFocusByBundleID(PWA_Aps.google_messages)
-end)
-
-hs.hotkey.bind(hyper, "u", function()
-	hs.application.launchOrFocus(Apps.spark)
-end)
-
-hs.hotkey.bind(hyper, "l", function()
-	hs.application.launchOrFocus(Apps.slack)
-end)
-
-hs.hotkey.bind(hyper, "m", function()
-	hs.application.launchOrFocus(Apps.meet)
-end)
-
-hs.hotkey.bind(hyper, "f", function()
-	hs.application.launchOrFocus(Apps.chatgpt)
-end)
-
-hs.hotkey.bind(hyper, "c", function()
-	hs.application.launchOrFocus(Apps.googlechat)
-end)
-
-hs.hotkey.bind(hyper, "d", function()
-	hs.application.launchOrFocus(Apps.discord)
-end)
-
-hs.hotkey.bind(hyper, "q", function()
-	hs.application.launchOrFocus(Apps.quicktime)
-end)
-
 hs.hotkey.bind(hyper, "a", function()
 	hs.application.launchOrFocus(Apps.alacritty)
 end)
+-- hs.hotkey.bind(hyper, "k", function()
+-- 	hs.application.launchOrFocusByBundleID(PWA_Aps.google_messages)
+-- end)
+hs.hotkey.bind(hyper, "k", function()
+	hs.application.launchOrFocus(Apps.googlechat)
 
-hs.hotkey.bind(hyper, "g", function()
-	hs.application.launchOrFocus(Apps.chrome)
+	-- small delay so the app is actually frontmost before typing
+	hs.timer.doAfter(0.12, function()
+		hs.eventtap.keyStroke({ "cmd", "shift" }, "k", 0)
+	end)
 end)
-
-hs.hotkey.bind(hyper, "w", function()
-	hs.application.launchOrFocus(Apps.whatsapp)
-end)
-
 hs.hotkey.bind(hyper, "s", function()
 	hs.application.launchOrFocus(Apps.spotify)
-end)
-
-hs.hotkey.bind(hyper, "t", function()
-	Translate.translateSelection("ko", "en")
-end)
-
--- Hyper + 4 -> Google Translate selected text (KO->EN)
-hs.hotkey.bind(hyper, "4", function()
-	Translate.translateSelection("en", "ko")
-end)
-
--- Hyper + 5 -> Google Translate selected text (AR->EN)
-hs.hotkey.bind(hyper, "5", function()
-	Translate.translateSelection("en", "ar")
 end)
 
 -- Hotkey: Hyper + I
@@ -77,7 +29,9 @@ hs.hotkey.bind(hyper, "i", sound.pickInputDevice)
 hs.hotkey.bind(hyper, "o", sound.pickOutputDevice)
 
 -- Hotkey to toggle Accessibility (Virtual) Keyboard using existing AppleScript
-local scriptPath = "/Users/jevans/.config/macos-automations/ToggleAccessibilityKeyboard.scpt"
+
+local home = os.getenv("HOME")
+local scriptPath = home .. "/.config/macos-automations/ToggleAccessibilityKeyboard.scpt"
 
 hs.hotkey.bind(hyper, "v", function()
 	local cmd = string.format('osascript "%s"', scriptPath)
--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -7,6 +7,21 @@ local home = os.getenv("HOME")
 -- Allow require("foo") to load ~/.config/hammerspoon/foo.lua
 package.path = package.path .. ";" .. home .. "/.config/hammerspoon/?.lua"
 
+hs.loadSpoon("Hammerflow")
+
+-- Hammerflow searches these in order and loads the first valid TOML.
+spoon.Hammerflow.loadFirstValidTomlFile({
+	"home.toml",
+	"work.toml",
+	"Spoons/Hammerflow.spoon/sample.toml",
+})
+
+-- Optional: if your TOML enables auto_reload, respect it.
+if spoon.Hammerflow.auto_reload then
+	hs.loadSpoon("ReloadConfiguration")
+	spoon.ReloadConfiguration:start()
+end
+
 require("apps")
 require("hotkeys")
 require("translate")
